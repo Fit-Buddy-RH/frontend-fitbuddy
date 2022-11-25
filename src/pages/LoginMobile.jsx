0856955b86b9495d53e6cbaad6f6ac4b93f13a69
LennyCocom <lennyinnelcocompedraza@gmail.com>
2022-11-25 09:25:22 -0600

    update buttons

--- src/pages/LoginMobile.jsx
+++ src/pages/LoginMobile.jsx
@@ -1,14 +1,29 @@
 import { DefaultLayout } from "../layouts/DefaultLayout"
+import { useGoogleLogin } from '@react-oauth/google';
 import { ReactComponent as FitbuddyIcon } from '../assets/FitbuddyIcon.svg'
-import { ReactComponent as FacebookIcon } from '../assets/FacebookIcon.svg'
+import { ReactComponent as GoogleIcon } from 
+'../assets/GoogleIconButton.svg'
+import { ReactComponent as FacebookIcon } from '../assets/FacebookIconButton.svg'
 
 export const LoginMobile = () => {
+  const googleLogin = useGoogleLogin({
+    // flow: "auth-code",
+    onSuccess: async (codeResponse) => {
+    console.log(codeResponse);
+    // const tokens = await axios.post("http://localhost:8080/auth/google", {
+    // code: codeResponse.code,
+    // });
+    
+    // console.log(tokens);
+    },
+    onError: (errorResponse) => console.log(errorResponse),
+    });
   return (
     <>
-      <body className="relative md:grid md:grid-cols-12">
-        <div className="h-screen bg-cover bg-login_image blur-sm md:col-span-6 md:h-screen  md:blur-none">
+      <body className="relative lg:grid lg:grid-cols-12">
+        <div className="h-screen bg-cover bg-login_image blur-sm lg:col-span-6 lg:h-screen  lg:blur-none">
         </div>
-        <div className=" absolute w-40 h-1/2 rounded inset-24  blur-none bg-black-700/[.06] absolute text-gray-50 md:bg-black-700 md:col-span-6 md:grid-cols-6 md:inset-0 md:static md:h-full md:w-full">
+        <div className="absolute w-3/4 h-auto rounded inset-10 flex flex-col justify-center items-center blur-none bg-black-700/[.06] absolute text-gray-50 lg:bg-black-700 lg:col-span-6 lg:grid-cols-6 lg:inset-0 lg:static lg:h-full md:w-auto lg:flex lg:flex-col lg:justify-center lg:items-center">
           <div className="flex flex-row mt-10">
             <FitbuddyIcon />
             <h1 className="p-2 text-gray-50 font-rubik text-2xl italic font-bold">Fitbuddy</h1>
@@ -17,23 +32,41 @@ export const LoginMobile = () => {
             Puedes iniciar sesión con alguno de los
             siguientes servicios:
           </div>
-          <button className="flex items-center
-          justify-evenly
+          <button className="flex 
+          items-center
           rounded-full
-          text-gray-50 w-3/4
-          bg-violet-700 my-4
+          text-center 
+          h-[45px]
+          text-base
+          italic
+          font-bold italic
+          text-gray-50 
+          w-[150px]
+          bg-violet-900 my-4
           filter-none
-          truncate">
-            <FacebookIcon />
+          truncate
+          overflow-auto
+          md:w-[185px]
+          lg:w-40">
+            <FacebookIcon className="m-2 md:m-4"/>
             Facebook</button>
-          <button className="flex items-center
-          justify-evenly
+          <button className="flex 
+          items-center
           rounded-full
-          text-gray-50 w-3/4
-          bg-violet-700 my-4
+          text-center 
+          h-[45px]
+          text-base
+          italic
+          font-bold italic
+          text-gray-50 
+          w-[150px]
+          bg-violet-900 my-4
           filter-none
-          truncate">
-            <FacebookIcon />
+          truncate
+          overflow-auto
+          md:w-[185px]
+          lg:w-40" onClick={()=>{googleLogin()}}>
+            <GoogleIcon className="m-2 md:m-4"/>
             Google</button>
 
           <div className="mt-2"><p className="text-gray-50 text-xs">¿No tienes una cuenta? <span className="text-violet-900 text-xs">Registrate</span></p></div>
