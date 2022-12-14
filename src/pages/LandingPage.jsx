4dcb47856a1a8827bf1c8eb570ec3be3e5177b6e
Rodrigo Montoya <montoyar32@gmail.com>
2022-12-14 17:54:52 -0500

    Bugs solved

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,6 +5,7 @@ import { ReactComponent as RunningIcon } from "../assets/RunningIcon.svg";
 import { ReactComponent as JoggingIcon } from "../assets/JoggingIcon.svg";
 import { ReactComponent as FitnessIcon } from "../assets/FitnessIcon.svg";
 
+
 export const LandingPage = () => {
   return (
     <div>
@@ -20,12 +21,9 @@ export const LandingPage = () => {
           </Link>
         </nav>
         <section className="flex sm:backdrop-blur-lg sm:w-5/12 flex-col h-5/6 rounded-2xl justify-center pt-36 p-8 xl:pl-32 gap-12 xl:gap-8">
-          <h1 className="italic text-center sm:text-left text-5xl lg:text-7xl font-rubik text-violet-900 font-bold">
-            FITBUDDY
-          </h1>
+          <h1 className="italic text-center sm:text-left text-5xl lg:text-7xl font-rubik text-violet-900 font-bold">FITBUDDY</h1>
           <h2 className="italic text-center sm:text-left text-xl 2xl:w-9/12 font-rubik text-gray-50">
-            ÚNETE PARA CONOCER, SOCIALIZAR Y PRACTICAR CON TU COMUNIDAD RUNNER
-            LOCAL
+            ÚNETE PARA CONOCER, SOCIALIZAR Y PRACTICAR CON TU COMUNIDAD RUNNER LOCAL
           </h2>
           <Link to="/login-2" className="self-center md:self-start">
             <button className="text-gray-50 transition font-rubik font-bold px-4 xl:text-xl italic bg-violet-900 hover:bg-violet-700 hover:shadow-md hover:shadow-violet-900 py-4 rounded-full">
@@ -37,12 +35,9 @@ export const LandingPage = () => {
       <div className="bg-black-700">
         <section className="flex w-screen flex-col md:flex-row md:h-860 justify-around items-center md:py-24 xl:px-24 2xl:px-80">
           <div className="flex flex-col md:w-4/12 gap-12 md:pl-12">
-            <h1 className="italic text-center sm:text-left text-5xl font-rubik mt-28 md:mt-0 text-violet-900 font-bold">
-              CONECTA
-            </h1>
+            <h1 className="italic text-center sm:text-left text-5xl font-rubik mt-28 md:mt-0 text-violet-900 font-bold">CONECTA</h1>
             <h2 className="text-center sm:text-left text-xl 2xl:w-9/12 mx-8 md:mx-0 font-rubik text-gray-50">
-              Conecta con corredores cercanos y crea tu propio evento deportivo
-              con fitbuddy
+              Conecta con corredores cercanos y crea tu propio evento deportivo con fitbuddy
             </h2>
             <Link to="/login-1" className="self-center md:self-start">
               <button className="text-gray-50 transition px-4 font-rubik font-bold italic bg-violet-900 hover:bg-violet-700 hover:shadow-md hover:shadow-violet-900 py-4 rounded-full">
@@ -55,9 +50,7 @@ export const LandingPage = () => {
         <section className="bg-gray-100 flex w-screen flex-col md:flex-row md:h-860 justify-around items-center md:py-24 xl:px-24 2xl:px-80">
           <JoggingIcon className="scale-p md:scale-75 lg:scale-125" />
           <div className="flex flex-col md:w-4/12 gap-12 md:pr-12">
-            <h1 className="italic text-center sm:text-left text-5xl font-rubik text-violet-900 font-bold">
-              CONOCE
-            </h1>
+            <h1 className="italic text-center sm:text-left text-5xl font-rubik text-violet-900 font-bold">CONOCE</h1>
             <h2 className="text-center sm:text-left text-xl 2xl:w-9/12 mx-8 md:mx-0 font-rubik text-black-700">
               Mantente en forma al compartir experiencias con amigos y conocidos
             </h2>
@@ -70,9 +63,7 @@ export const LandingPage = () => {
         </section>
         <section className="flex w-screen flex-col md:flex-row md:h-860 justify-around items-center md:py-24 xl:px-24 2xl:px-80">
           <div className="flex flex-col md:w-4/12 gap-12 md:pl-12">
-            <h1 className="italic text-center sm:text-left text-5xl font-rubik mt-28 md:mt-0 text-violet-900 font-bold">
-              ENCUENTRA
-            </h1>
+            <h1 className="italic text-center sm:text-left text-5xl font-rubik mt-28 md:mt-0 text-violet-900 font-bold">ENCUENTRA</h1>
             <h2 className="text-center sm:text-left text-xl 2xl:w-9/12 mx-8 md:mx-0 font-rubik text-gray-50">
               Encuentra el grupo de running ideal para ti sin salir de casa
             </h2>
